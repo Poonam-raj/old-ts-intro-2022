9df96aa451b1694c89b90011cc4e1139bfa003f5
Poonam <p_rajput@hotmail.co.uk>
2023-01-17 12:31:35 +0000

    complete section 1

---
+++ src/problems/01_definate_assignment/problem_1.js
@@ -0,0 +1,5 @@
+var greeting = 'Good morning typescript enthusiast';
+var shoutGreeting = function (greet) {
+    console.log(greet.toUpperCase());
+};
+shoutGreeting(greeting);
---
+++ src/problems/01_definate_assignment/problem_2.js
@@ -0,0 +1,6 @@
+var rad = 4;
+var calculateAreaOfCircle = function (radius) {
+    var pi = 3.14;
+    return pi * (radius * radius);
+};
+calculateAreaOfCircle(rad);
